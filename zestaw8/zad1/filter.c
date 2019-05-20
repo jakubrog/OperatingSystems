423854ce8f1e633ee24be9472ad27ece847ecfec
jakubrog <jrog25@student.agh.edu.pl>
2019-05-20 12:49:56 +0200

    8th done

--- zestaw8/zad1/filter.c
+++ zestaw8/zad1/filter.c
@@ -1,37 +1,174 @@
 #include "filter.h"
 
 
-void save_picture(int w, int h, int **out_pict_matrix, char *path) {
-    printf("hereisok\n");
-    FILE *fp = fopen(path, "w+");
-    if(!fp){
-      printf("Cannot open file %s\n", path );
-      exit(1);
-    }
-    char buff[1024];
-    fprintf(fp, "P2\n");
-    fprintf(fp, "%d %d\n", w, h);
-    fprintf(fp, "%d\n", 255);
+int **image = NULL;
+int i_height;
+int i_width;
 
-    for (int i = 0; i < h; i++) {
-        for (int j = 0; j < w; j++) {
-            if (j < w - 1) {
-                sprintf(buff, "%d ", out_pict_matrix[i][j]);
-            } else {
-                sprintf(buff, "%d\n", out_pict_matrix[i][j]);
-            }
-            fputs(buff, fp);
+double **filter = NULL;
+int f_size;
+int **result = NULL;
+
+int thread_count;
+int *thread_no;
+
+
+void clear(){
+  int i;
+    if(image != NULL){
+        for(i = 0; i < i_height; i++)
+            free(image[i]);
+        free(image);
+    }
+    if(result != NULL){
+        for(i = 0; i < i_height; i++)
+          free(result[i]);
+        free(result);
+    }
+    if(filter != NULL){
+      for(i = 0; i < f_size; i++)
+          free(filter[i]);
+      free(filter);
+  }
+}
+
+int calculate_pixel(int x, int y){
+    int i,j;
+    double sum = 0;
+    for(i = 0; i < f_size; i++){
+        for(j = 0; j < f_size; j++){
+            int h = max(0, x - ceil(f_size / 2.0) + i);
+            int w = max(0, y - ceil(f_size / 2.0) + j);
+            sum += h < i_height && w < i_width ? image[h][w] * filter[i][j] : 0;
         }
     }
-    fclose(fp);
+    return (int)round(sum);
+}
+
+void print_image(int **array, int w, int h){
+      int index = 0;
+      for(int i = 0; i < h; i++){
+        for(int j = 0; j < w; j++){
+          printf("%d  ", array[i][j]);
+          index++;
+          if(index >= 70){
+            printf("\n");
+            index = 0;
+          }
+        }
+      }
+}
+
+void *block_thread_start(void *arg){
+    int *no = (int*)arg;
+    int k = no[0];
+
+    double *ret_val = malloc(sizeof(double));
+    int x_min = (k-1) * ceil(i_width / (1.0*thread_count));
+    int x_max = (k) * ceil(i_width / (1.0*thread_count)) - 1;
+
+    clock_t t;
+    t = clock();
+
+    for(int i = x_min; i <= x_max; i++){
+           for(int j = 0; j < i_height; j++){
+               result[j][i] = calculate_pixel(i,j);
+           }
+     }
+
+    t = clock() - t;
+    *ret_val = ((double)t)/CLOCKS_PER_SEC;
+    pthread_exit(ret_val);
 }
 
 
-struct image read_image(char *path){
+void *interleaved_thread_start(void *arg){
+  int *no = (int*)arg;
+  int k = no[0];
 
-    struct image result;
+  double *ret_val = malloc(sizeof(double));
 
+  clock_t t;
+  t = clock();
+
+  for(int i = k-1; i < i_width; i += k){
+     for(int j = 0; j < i_height; j++){
+        result[j][i] = calculate_pixel(i, j);
+     }
+ }
+
+  t = clock() - t;
+  *ret_val = ((double)t)/CLOCKS_PER_SEC;
+  pthread_exit(ret_val);
+}
+
+int main(int argc, char **argv){
+    atexit(clear);
+
+    if(argc < 6){
+        printf("Not enough arguments");
+        exit(1);
+    }
+
+    thread_count = atoi(argv[1]);
+
+    read_image(argv[2]);
+    read_filter(argv[3]);
+
+    result = calloc(i_height, sizeof(int*));
+
+    for(int i = 0; i< i_height; i++)
+       result[i] = calloc(i_width, sizeof(int));
+
+
+    double *time_results[thread_count];
+    thread_no = malloc(thread_count*sizeof(int));
+    for(int i = 0; i < thread_count; i++)
+        thread_no[i] = i + 1;
+
+    pthread_t thread[thread_count];
+
+    clock_t t;
+
+    t = clock();
+
+    if(strcmp(argv[4], BLOCK) == 0){
+        for(int i = 0; i < thread_count; i++){
+            pthread_create(&thread[i],NULL,block_thread_start, &thread_no[i]);
+        }
+    }else if(strcmp(argv[4], INTERLEAVED) == 0){
+        for(int i = 0; i < thread_count; i++){
+            pthread_create(&thread[i],NULL,interleaved_thread_start, &thread_no[i]);
+        }
+    }else{
+      printf("Wrong mode\n");
+      exit(1);
+    }
+
+    save_result_picture(argv[5]);
+
+    for(int i = 0; i < thread_count; i++){
+      pthread_join(thread[i], (void*)&time_results[i]);
+    }
+
+    t = clock() - t;
+    printf("Mode: %s\n", argv[4]);
+    printf("No of threads: %d\n", thread_count);
+    printf("Filter dimensions: %d x %d\n", f_size, f_size);
+    printf("Image dimensions: %d x %d\n", i_height, i_width);
+    printf("Summary time: %fs\nThreads times:\n", ((double)t)/CLOCKS_PER_SEC);
+    for(int i = 0; i < thread_count; i++){
+      printf("\t%d. %fs\n",i+1, *time_results[i] );
+    }
+    printf("\n\n\n");
+
+    return 0;
+}
+
+
+void read_image(char *path){
     FILE *file_in = fopen(path, "r+");
+
     if(!file_in){
         printf("Cannot open file %s\n", path);
         exit(1);
@@ -46,11 +183,10 @@ struct image read_image(char *path){
     dimensions = strdup(buff);
     int width =  (int) strtol(strsep(&dimensions, " \t"), NULL, 10);
     int height = (int) strtol(strsep(&dimensions, " \t"), NULL, 10);
-    printf("h = %d w = %d\n",height, width);
+    int **picture = calloc(height, sizeof(int*));
 
-    double **picture = calloc(height, sizeof(double*));
     for(int i = 0; i< height; i++)
-      picture[i] = calloc(width, sizeof(double));
+      picture[i] = calloc(width, sizeof(int));
 
     fgets(buff, BUFF_SIZE, file_in); // skip line
 
@@ -59,7 +195,7 @@ struct image read_image(char *path){
     while (fgets(buff, BUFF_SIZE, file_in) != NULL) {
         char *number = strtok(buff, " \n\t\r");
         while (number != NULL) {
-            picture[y][x] = (double)strtol(number, NULL, 10);
+            picture[y][x] = strtol(number, NULL, 10);
             x++;
             if (x == width) {
                 y++;
@@ -70,107 +206,77 @@ struct image read_image(char *path){
     }
 
     fclose(file_in);
-    result.width = width;
-    result.height = height;
-    result.image = picture;
-
-    return result;
-}
-
-clock_t sub_time(clock_t start, clock_t end) {
-    return (end - start) / CLOCKS_PER_SEC;
-}
-
-void *thread_start(void *arg){
-
-    struct thread_arg *t_arg = (struct thread_arg*) arg;
-    struct thread_info *info = t_arg->info;
-
-    int k = t_arg->thread_no;
-    int m = info->threads_no;
-    int n = info->input.width;
-    int x, y;
-    clock_t start, end;
-    start = clock();
-     for( x = k ; x < k + 1 ; x++)
-       for(y = 0 ; y < info->input.height; y++){
-          info->result_image[y][x] = 200;
-    }
-    end = clock();
-    return (end - start) / CLOCKS_PER_SEC;
-
+    i_height = height;
+    i_width = width;
+    image = picture;
 }
 
 
-int single_pixel(int x, int y, struct image pictrue, struct image filter) {
-    double result = 0;
-    int c = filter.height;
-    int _x, _y;
-    /*
-    for(int i = 0; i < c; i++)
-        for(int j = 0; j < c; j++){
-          _y = max(1, x-ceil((double)c/2) + i);
-          _x = max(1, y-ceil((double)c/2) + j);
-          result += pictrue.image[_y][_x] * filter.image[i][j];
-    }
-    */
+void read_filter(char *path){
+  FILE *file_in = fopen(path, "r+");
 
-    return (int)round(pictrue.image[y][x] * 0.5) % 255;
+  if(!file_in){
+      printf("Cannot open file %s\n", path);
+      exit(1);
+  }
+
+  char buff[BUFF_SIZE];
+  fgets(buff, BUFF_SIZE, file_in); // skip first line
+  fgets(buff, BUFF_SIZE, file_in); // load dimensions
+
+  char *dimensions;
+
+  dimensions = strdup(buff);
+  int width =  (int) strtol(strsep(&dimensions, " \t"), NULL, 10);
+  int height = (int) strtol(strsep(&dimensions, " \t"), NULL, 10);
+  double **picture = calloc(height, sizeof(double*));
+
+  for(int i = 0; i< height; i++)
+    picture[i] = calloc(width, sizeof(double));
+
+  fgets(buff, BUFF_SIZE, file_in); // skip line
+
+  int x = 0;
+  int y = 0;
+  while (fgets(buff, BUFF_SIZE, file_in) != NULL) {
+      char *number = strtok(buff, " \n\t\r");
+      while (number != NULL) {
+          picture[y][x] = atof(number); // atof - convert string to double
+          x++;
+          if (x == width) {
+              y++;
+              x = 0;
+          }
+          number = strtok(NULL, " \t\n\r");
+      }
+  }
+
+  fclose(file_in);
+  f_size = width;
+  filter = picture;
+
 }
 
-
-int main(int argc, char **argv){
-
-    int errno;
-    if(argc < 2){
-        handle_error("Not enough no. of arguments");
+void save_result_picture(char *path) {
+    FILE *fp = fopen(path, "w+");
+    if(!fp){
+      printf("Cannot open file %s\n", path );
+      exit(1);
     }
+    char buff[1024];
+    fprintf(fp, "P2\n");
+    fprintf(fp, "%d %d\n", i_width, i_height);
+    fprintf(fp, "%d\n", 255);
 
-    struct thread_info *t_info = malloc(sizeof(struct thread_info));
-    t_info->threads_no = atoi(argv[1]);
-    t_info->input = read_image(argv[2]);
-    t_info->filter = read_image(argv[3]);
-
-
-    int **result = calloc(t_info->input.height, sizeof(int*));
-    for(int i = 0; i< t_info->input.height; i++)
-       result[i] = calloc(t_info->input.width, sizeof(int));
-
-    t_info->result_image = result;
-    t_info->mode = argv[4]; // BLOCK or INTERLEAVED
-
-    double time_results[t_info->threads_no];
-
-    pthread_t thread[t_info->threads_no];
-    struct thread_arg t_arg[t_info->threads_no];
-
-    for(int i = 0; i< t_info->threads_no; i++){
-       t_arg[i].thread_no = i;
-       t_arg[i].info = t_info;
+    for (int i = 0; i < i_height; i++) {
+        for (int j = 0; j < i_width; j++) {
+            if (j < i_width - 1) {
+                sprintf(buff, "%d ", result[i][j]);
+            } else {
+                sprintf(buff, "%d\n", result[i][j]);
+            }
+            fputs(buff, fp);
+        }
     }
-
-    clock_t start, end;
-    start = clock();
-
-    for(int i = 0; i < t_info->threads_no; i++){
-        pthread_create(&thread[i], NULL, thread_start, &t_arg[i]);
-        printf("creating %d\n",i );
-        usleep(10);
-    }
-    for(int i = 0; i<t_info->input.height; i++ ){
-      for(int j = 0; j< t_info->input.width;j++)
-          printf("%d ", t_info->result_image[i][j]);
-      printf("\n");
-    }
-  //  save_picture(t_info->input.height, t_info->input.width, t_info->result_image, "test.pgm");
-    sleep(2);
-    end = clock();
-
-    for(int i = 0; i < t_info->threads_no; i++){
-        pthread_join(thread[i], (void**)&time_results[i]);
-        printf("Thread no. time: %ld\n", time_results[i]);
-    }
-
-    printf("Main time: %ld \n", (end-start));
-    return 0;
+    fclose(fp);
 }
--- zestaw8/zad1/filter.h
+++ zestaw8/zad1/filter.h
@@ -1,3 +1,4 @@
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -8,43 +9,21 @@
 #include <ctype.h>
 #include <math.h>
 
-#define handle_error_en(en, msg) \
-        do { errno = en; perror(msg); exit(EXIT_FAILURE); } while (0)
-
-#define handle_error(msg) \
-        do { perror(msg); exit(EXIT_FAILURE); } while (0)
-
 #define BUFF_SIZE 512
-
 #define BLOCK "block"
 #define INTERLEAVED "interleaved"
+
 #define max(a,b) \
    ({ __typeof__ (a) _a = (a); \
        __typeof__ (b) _b = (b); \
      _a > _b ? _a : _b; })
 
-struct thread_arg{
-  int thread_no;
-  struct thread_info *info;
-} thread_arg;
-
-struct image{
-  double **image;
-  int width;
-  int height;
-}image;
-
-struct thread_info{
-  int threads_no;
-  struct image input;
-  int **result_image;
-  struct image filter;
-  char *mode;
-}thread_info;
 
 
-int single_pixel(int x, int y, struct image pictrue, struct image filter);
-struct image read_image(char *path);
-void *thread_start(void *arg);
-void elapsed_time(clock_t stat, clock_t end);
+void read_image(char *path);
+void *interleaved_thread_start(void *arg);
+void *block_thread_start(void *arg);
 void write_results(char *path, char *to_write);
+void save_result_picture( char *path);
+int calculate_pixel(int x, int y);
+void read_filter(char *file_path);
